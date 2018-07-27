07af173ed170717065ae733886fcc88e2df83764
Mark Acosta <markjacobacosta@gmail.com>
2018-07-27 18:58:25 -0400

    Updates made to fulfill project task, tests created for units and functions with Enzyme, some slight styles updates

--- summing-grid/src/App.js
+++ summing-grid/src/App.js
@@ -2,12 +2,83 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+      values: [0,0,0]
+  }
+
+  numberFormatter = (num) => { //checks for turn of denomination against its 100s place to decide whether a decimal should be provided and which abbreviation to use
+    const toFixedWithoutRounding = numToFix => numToFix.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]; //returns matched number up to second decimal place without round
+
+  	if(num < 1000) {
+  		return num;
+  	}
+
+    if(num < 10000 && num !== 1000) {
+      return toFixedWithoutRounding(num/1000) + "K";
+    }
+
+  	if(num < 1000000) {
+  		return Math.round(num/1000) + "K";
+  	}
+
+  	if(num < 10000000 && num !== 1000000) {
+  		return toFixedWithoutRounding(num/1000000) + "M";
+  	}
+
+  	if(num < 1000000000) {
+  		return Math.round((num/1000000)) + "M";
+  	}
+
+    if(num < 10000000000 && num !== 1000000000) {
+      return toFixedWithoutRounding(num/1000000000) + "B";
+    }
+
+  	if(num < 1000000000000) {
+  		return Math.round((num/1000000000)) + "B";
+  	}
+
+  	return "1T+"; // default past 1 trillion
+  }
+
+  validateNumericalInput = (value) => { // checks that the input is a number and returns alert if invalid input
+    if (!value.match(/^[0-9]+$/) && value !== "") {
+       alert("Must input numbers");
+       return false;
+    }
+
+    return true;
+  }
+
+  updateNumber = (e, index) => { //onchange listener for inputs, updates state array with corresponding input index
+    const value = e.target.value;
+
+    if(this.validateNumericalInput(value)) {
+      const values = this.state.values;
+      values[index] = value * 1; //forces numerical value by multiplying by 1
+      this.setState({values});
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="gridbox">
+          <span>Num1</span>
+          <input onChange={(e) => this.updateNumber(e, 0)} type="text" value={this.state.values[0]} />
+        </div>
+        <div className="gridbox">
+          <span>Num2</span>
+          <input onChange={(e) => this.updateNumber(e, 1)} type="text" value={this.state.values[1]} />
+        </div>
+        <div className="gridbox">
+          <span>Num3</span>
+          <input onChange={(e) => this.updateNumber(e, 2)} type="text" value={this.state.values[2]} />
+        </div>
+        <div className="gridbox">
+          <span>Sum</span>
+          {this.numberFormatter(this.state.values[0] + this.state.values[1] + this.state.values[2])}
+        </div>
       </div>
     );
   }
--- summing-grid/src/App.test.js
+++ summing-grid/src/App.test.js
@@ -1,9 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {configure, shallow, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+
+// tslint:disable-next-line:no-any
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('testing validateNumericalInput function against different input values', () => {
+  const instance = shallow(<App />).instance();
+
+  const values = [
+    {input: "1", output: true},
+    {input: "12345", output: true},
+    {input: "0928340923840", output: true},
+    {input: "", output: true},
+    {input: "1a", output: false},
+    {input: "1!", output: false},
+    {input: "Hello World!", output: false},
+    {input: "  ", output: false}
+  ];
+
+  values.forEach((value) => {
+    expect(instance.validateNumericalInput(value.input)).toBe(value.output);
+  });
+});
+
+
+it('testing numberFormatter function against different input values', () => {
+  const instance = shallow(<App />).instance();
+
+  const values = [
+    {input: "1", output: "1"},
+    {input: "999", output: "999"},
+    {input: "1000", output: "1K"},
+    {input: "100000", output: "100K"},
+    {input: "1000000", output: "1M"},
+    {input: "100000000", output: "100M"},
+    {input: "1234", output: "1.23K"},
+    {input: "123400", output: "123K"},
+    {input: "1234000", output: "1.23M"},
+    {input: "1234000000", output: "1.23B"},
+    {input: "1234000000000", output: "1T+"}
+  ];
+
+  values.forEach((value) => {
+    expect(instance.numberFormatter(value.input)).toBe(value.output);
+  });
+});
+
+it('testing sum of inputs values by leveraging updateNumber function called by onChange listeners', () => {
+  const instance = shallow(<App />).instance();
+
+  let sum = instance.state.values[0] + instance.state.values[1] + instance.state.values[2];
+  let formattedSum = instance.numberFormatter(sum);
+
+  expect(sum).toBe(0);
+  expect(formattedSum).toBe(0)
+
+  instance.updateNumber({target: { value: "5"}, preventDefault: () => {}}, 0);
+  instance.updateNumber({target: { value: "10"}, preventDefault: () => {}}, 1);
+  instance.updateNumber({target: { value: "25"}, preventDefault: () => {}}, 2);
+
+  expect(instance.state.values).toEqual([5, 10, 25]);
+
+  sum = instance.state.values[0] + instance.state.values[1] + instance.state.values[2];
+  formattedSum = instance.numberFormatter(sum);
+
+  expect(formattedSum).toBe(40);
+
+  instance.updateNumber({target: { value: "5000"}, preventDefault: () => {}}, 0);
+  instance.updateNumber({target: { value: "1000000"}, preventDefault: () => {}}, 1);
+  instance.updateNumber({target: { value: "25000"}, preventDefault: () => {}}, 2);
+
+  expect(instance.state.values).toEqual([5000, 1000000, 25000]);
+
+  sum = instance.state.values[0] + instance.state.values[1] + instance.state.values[2];
+  formattedSum = instance.numberFormatter(sum);
+
+  expect(formattedSum).toBe("1.03M");
 });
